567d5665c8f256594b946af3a5f6e11f0916c011
pyetro-cod <pyetro.f@aluno.uepb.edu.br>
2025-11-12 19:47:15 -0300

    ContaNumPar

---
+++ ContaNumPar/index.js
@@ -0,0 +1,10 @@
+function numPar (n){
+    let totPar = 0;
+    for(let num of n){
+        if(num % 2 === 0){
+            totPar ++
+        }
+    }
+    return totPar;
+}
+console.log(numPar([2,3,7,8,]))
