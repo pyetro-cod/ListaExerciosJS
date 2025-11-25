be195b1c1b766c0d57ce7ec1ddc57cc9aae818e3
pyetro-cod <pyetro.f@aluno.uepb.edu.br>
2025-11-25 14:21:05 -0300

    Trocar imagem

---
+++ DomAtualizarImage/index.js
@@ -0,0 +1,6 @@
+const img = document.getElementById('image');
+const button = document.getElementById('btn')
+
+button.addEventListener('click', () => {
+    img.src = 'https://images.pexels.com/photos/34803971/pexels-photo-34803971.jpeg'
+})
